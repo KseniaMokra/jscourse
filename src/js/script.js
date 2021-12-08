851d05767f5277ea562ae07329e8841b34cb3825
Ksenia <kseniya.mokraya@gmail.com>
2021-12-08 13:36:20 +0200

    first app

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,25 @@
 "use strict";
 
-let user = prompt('What is your name?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-alert(`Hello ${user}!`);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
+      rating = prompt('На сколько оцените его?', ''),
+      lastFilm2 = prompt('Один из последних просмотренных фильмов?', ''),
+      rating2 = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[lastFilm] = rating;
+personalMovieDB.movies[lastFilm2] = rating2;
+
+console.log(personalMovieDB);
+
+// let user = prompt('What is your name?', '');
+
+// alert(`Hello ${user}!`);
